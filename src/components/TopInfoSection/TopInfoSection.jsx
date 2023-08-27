1fe8cf4c459494bdf8726c58c9173ae9bc167fb7
Alvin Hart <pageking123@gmail.com>
2023-08-27 12:35:15 +0900

    Make small changes

---
+++ src/components/TopInfoSection/TopInfoSection.jsx
@@ -0,0 +1,11 @@
+import "../css/Top.css"
+
+function TopInfoSection({ name, contactInfo }) {
+    return (
+        <div className="top">
+
+        </div>
+    )
+}
+
+export default TopInfoSection;
