893e7d7a482bd8cc83df25b52615bff3d645e4de
Alvin Hart <pageking123@gmail.com>
2023-09-04 02:29:35 +0900

    Edit workInfo css and workInfo component to add DeleteButton

--- src/components/MainInfoSection/WorkSection/WorkInfo.jsx
+++ src/components/MainInfoSection/WorkSection/WorkInfo.jsx
@@ -2,10 +2,12 @@ import './WorkInfo.css'
 import TimeLength from '../../UtilityComponents/TimeLength'
 import Input from "../../UtilityComponents/Input"
 import WorkAchievementList from './WorkAchievementList'
+import DeleteButton from '../../UtilityComponents/DeleteButton'
 
 function WorkInfo({company, position, onChange, startTime, endTime, workAchievementList, hasSubmit }){
   return(
         <li className="workInfo">
+          <DeleteButton/>
           <Input
             id="company"
             value={company}
