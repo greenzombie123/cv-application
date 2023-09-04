e974fd8835af0dbf3b85a73521729b97533d1f62
Alvin Hart <pageking123@gmail.com>
2023-09-04 09:32:10 +0900

    Components inside WorkInfo are now passed as a children prop to avoid too many props

--- src/components/MainInfoSection/WorkSection/WorkSection.jsx
+++ src/components/MainInfoSection/WorkSection/WorkSection.jsx
@@ -1,14 +1,34 @@
-import WorkInfo from './WorkInfo'
-import './WorkSection.css'
+import WorkInfo from "./WorkInfo";
+import TimeLength from "../../UtilityComponents/TimeLength";
+import Input from "../../UtilityComponents/Input";
+import WorkAchievementList from "./WorkAchievementList";
+import DeleteButton from "../../UtilityComponents/DeleteButton";
+import "./WorkSection.css";
 
-function WorkSection({hasSubmit, workInfoList}){
-    return(
-        <ul className='workSection'>
-            {workInfoList.map(workInfo => {
-                return (<WorkInfo key={workInfo.id} {...workInfo}/>)
-            })}
-        </ul>
-    )
+function WorkSection({ hasSubmit, workInfoList }) {
+  return (
+    <ul className="workSection">
+      {workInfoList.map((workInfo) => {
+        return (
+          <WorkInfo key={workInfo.id}>
+            <DeleteButton />
+            <Input
+              id="company"
+              value={workInfo.company}
+              placeholder="Company Name"
+            />
+            <Input
+              id="position"
+              value={workInfo.position}
+              placeholder="Position"
+            />
+            <TimeLength startTime={workInfo.startTime} endTime={workInfo.endTime} />
+            <WorkAchievementList workAchievementList={workInfo.workAchievementList} />
+          </WorkInfo>
+        );
+      })}
+    </ul>
+  );
 }
 
-export default WorkSection
+export default WorkSection;
