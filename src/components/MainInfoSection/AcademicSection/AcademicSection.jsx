2495a5b117792dfc0af8f4e242f5d20b6024407b
Alvin Hart <pageking123@gmail.com>
2023-09-07 08:09:50 +0900

    Add Button in Academic Section now disappears if hasSubmit is true

--- src/components/MainInfoSection/AcademicSection/AcademicSection.jsx
+++ src/components/MainInfoSection/AcademicSection/AcademicSection.jsx
@@ -1,25 +1,31 @@
 import SchoolInfo from "./SchoolInfo";
 import { useSchoolInfoList } from "../../hooks/useSchoolInfoList";
 import "./AcademicSection.css";
+import { useHasSubmit } from "../../../hasSubmitContext";
+import { useContext } from "react";
 
 function AcademicSection() {
   const { schoolInfoList, onInputChange, addNewSchoolInfo, deleteSchoolInfo } =
     useSchoolInfoList();
+  const { HasSubmitContext } = useHasSubmit();
+  const hasSubmit = useContext(HasSubmitContext);
 
   return (
     <div className="academicSection_container">
       <h1>Academic History</h1>
       <ul className="academicSection_list">
         {schoolInfoList.map((ai) => (
-            <SchoolInfo
-              key={ai.id}
-              {...ai}
-              onInputChange={onInputChange(ai.id)}
-              onDeleleButtonClick={deleteSchoolInfo(ai.id)}
-            />
+          <SchoolInfo
+            key={ai.id}
+            {...ai}
+            onInputChange={onInputChange(ai.id)}
+            onDeleleButtonClick={deleteSchoolInfo(ai.id)}
+          />
         ))}
       </ul>
-      <button onClick={addNewSchoolInfo}>Add School Entry</button>
+      {!hasSubmit && (
+        <button onClick={addNewSchoolInfo}>Add School Entry</button>
+      )}
     </div>
   );
 }
