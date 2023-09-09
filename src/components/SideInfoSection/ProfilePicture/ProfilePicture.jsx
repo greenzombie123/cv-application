84d6e59d94ec1997c762e9cdbc67eeee1df93305
Alvin Hart <pageking123@gmail.com>
2023-09-09 16:02:37 +0900

    Implement addPicture functionality v2

--- src/components/SideInfoSection/ProfilePicture/ProfilePicture.jsx
+++ src/components/SideInfoSection/ProfilePicture/ProfilePicture.jsx
@@ -3,13 +3,18 @@ import "./ProfilePicture.css";
 import { useProfilePicture } from "../../hooks/useProfilePicture";
 
 function ProfilePicture() {
-    const {profilePicture, addPicture} = useProfilePicture() 
+  const { profilePicture, addPicture } = useProfilePicture();
   return (
     <div className="profilePicture">
       <img className="profilePicture_img" src={profilePicture} alt="" />
       <div className="profilePicture_button">
-        <label htmlFor="picture">+</label>
-        <input className="profilePicture_filePicker" type="file" id="picture" onChange={addPicture}/>
+        <label htmlFor="picture">{profilePicture ? "⟳" : "+"}</label>
+        <input
+          className="profilePicture_filePicker"
+          type="file"
+          id="picture"
+          onChange={addPicture}
+        />
       </div>
     </div>
   );
